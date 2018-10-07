974ee8203670ba546605da6f90008c1acfb655ae
Razvan Gabriel <razvangabriel9921@gmail.com>
2018-10-07 15:55:13 +0300

    deleted comments

--- ProductTest/UnitTest1.cs
+++ ProductTest/UnitTest1.cs
@@ -1,6 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Product;
-// aici ii dai namespace-ul pe care il ai in clasa pe care o vrei; namespace Product
 
 namespace ProductTest
 {
@@ -11,9 +10,6 @@ namespace ProductTest
         public void TestMethod1()
         {
             var product = new ProductEntity(){
-            //  stai sa vad daca recunosate clasa la build
-                Name = "Vlad"
-                //trebuie sa le scrii la mana ca asta e prost, dar recunoaste la build deci e bine sp or 
             };
         }
     }

dd7d7f74e851013343b32514c8211b9ca025d3d0
Razvan Gabriel <razvangabriel9921@gmail.com>
2018-10-07 17:27:42 +0300

    Added employee logic beeing generic

---
+++ Product/BusinessLayer/EmployeeLogic.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Product
+{
+    public class EmployeeLogic
+    {
+        private EmployeeEntity _employee;
+
+        public EmployeeLogic(EmployeeEntity employee)
+        {
+            _employee = employee ?? throw new ArgumentNullException(nameof(employee));
+        }
+
+        public string GetFullName()
+        {
+            return _employee.FirstName + " " + _employee.LastName;
+        }
+
+        public bool isActive()
+        {
+            var isActive = (_employee.EndDate > _employee.StartDate) ? true : false;
+
+            return isActive;
+        }
+
+    }
+}
